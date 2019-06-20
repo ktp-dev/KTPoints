511880b762f392632234beaecf3d1872112b6ad0
sletap <sletap@umich.edu>
2019-06-19 22:27:38 -0500

    Add basic events with transitions

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import LoginSignup from './views/LoginSignup.vue'
 import Landing from './views/Landing.vue'
+import Events from './views/Events.vue'
+// import EventCard from './components/EventCard.vue'
 
 Vue.use(Router)
 
@@ -22,6 +24,11 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       // component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    },
+    {
+      path: '/events/',
+      name: 'event',
+      component: Events
+    },
   ]
 })
---
+++ src/views/Events.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="section">
+    <div class="columns is-centered">
+      <div v-for="eventinfo in events" :key="eventinfo.location" class="column is-3">
+        <EventCard v-bind='eventinfo'/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue';
+export default {
+  data () {
+    return {
+      events: 
+      [
+        {
+          event: "Ethics Meeting",
+          location: 'North Quad 2486',
+          time: '8:30 PM',
+          date: '2/17/19',
+          points: 20,
+          description: 'Lorem hipsum flexatarian'
+        },
+        {
+          event: "Vue Workshop",
+          location: 'East Quad 1776',
+          time: '4:30 PM',
+          date: '6/19/19',
+          points: 15,
+          description: 'Lorem impsum dollar sign'
+        },
+      ]
+    }
+  },
+  components: {
+    EventCard
+  }
+}
+</script>

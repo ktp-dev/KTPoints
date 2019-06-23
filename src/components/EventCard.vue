7da6df79e5b5b9a00a17b987040a32ea0ddee7e5
sletap <sletap@umich.edu>
2019-06-23 14:15:56 -0500

    Add event animation

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,18 +1,23 @@
 <template>
-  <div v-on:click="expanded = !expanded" class="card has-text-centered">
+  <div v-on:click="toggle()" class="card has-text-centered">
     <header class="card-header title">
-      {{event}}
+      <p>{{event}}</p>
+      <div v-bind:class="{'expanded': expanded}" class="icon subtitle is-medium">
+        <i class="fas fa-angle-down"></i> 
+      </div>
     </header>
+
     <div class="card-content">
       <p> <strong>{{location}}</strong>  </p>
       <p> <strong>{{time}} {{date}}</strong> </p>
       <p> <strong>{{points}} Points</strong> </p>
-        <div v-if="expanded">
-          <br>
-          <p class='title'>About</p>
-          <p> {{description}} </p>
-        </div>
+      <div v-if="expanded">
+        <br>
+        <p class='title'>About</p>
+        <p> {{description}} </p>
+      </div>      
     </div>
+
   </div>
 </template>
 
@@ -24,6 +29,11 @@ export default {
   data(){
     return {
       expanded: false,
+    }
+  },
+  methods: {
+    toggle: function(){
+      this.expanded = !this.expanded;
     }
   },
   mounted() {
@@ -41,4 +51,14 @@ export default {
     align-items: center;
   }
 }
+
+div .fa-angle-down {
+    transition: all 1s cubic-bezier(0.165, 0.84, 0.44, 1);
+    transform: none;
+}
+
+div.expanded .fa-angle-down {
+    transform: rotate(180deg);
+}
+
 </style>

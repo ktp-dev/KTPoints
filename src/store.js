24f8d3810e23377be53a52ee355bdb909c94b03f
sletap <sletap@umich.edu>
2019-07-15 23:12:02 -0500

    Storing the user object

--- src/store.js
+++ src/store.js
@@ -5,15 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    user: null
   },
+
+  // Change state by providing state at the minimum
   mutations: {
-
+    addUser(state, currentUser){
+      state.user = currentUser;
+    }
   },
-  getters: {
 
+  getters: {
+    
   },
   
+  // Use actions to perform asynchronous calls (not possible in a mutation)
   actions: {
 
   }

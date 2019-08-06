7251adc8c1df9997fcdd17b74f80bfc63c3ba404
Neal Matta <nealmatt@umich.edu>
2019-08-06 14:35:22 -0400

    Layout changed, everything still works, just need to upload a new file to change the image

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,96 +1,109 @@
 <template>
 <transition name="slide-right" mode="out-in">
 <section class="section" id = 'UserInfo'>
-    <div class="columns is-centered">
-    <div class="column is-three-fifths">
-    <div class="card is-rounded">
+    <div class="card" id="user-container">
+    <div class="columns is-centered is-vcentered">
+    <div class="column is-two-fifths">
         <div class="card-image">
             <figure class="image is-square">
                 <img :src= "payload.imageURL">
             </figure>
         </div>
-    <div class="card-content is-small">
-        <div class="media">
-            <div class="columns is-mobile is-centered">
-                <div class="column">
-                    <div class="media-content">
-                        <p class="title fs-s3 is-capitalized">{{ payload.name }}</p>
-                        <div v-if="this.editing">
-                            <p class="subtitle fs-s4 has-text-weight-bold">
-                                 <span v-if="!isAlum">Year: </span>{{ payload.year }}
-                            </p>
-                        </div>
-                        <div v-else class="control columns">
-                            <p class="subtitle fs-s4 column has-text-weight-bold">
-                                 <span v-if="!isAlum">Year: </span>
-                            </p>
-                            <div class="select column">
-                                <select v-model="payload.year">
-                                    <option>Freshmen</option>
-                                    <option>Sophmore</option>
-                                    <option>Junior</option>
-                                    <option>Senior</option>
-                                    <option>Alumni</option>
-                                </select>
+        <div class="button-links field is-grouped is-grouped-centered buttons are-large">
+            <a v-bind:href="messengerURL" class=" button is-rounded is-info"target="_blank">
+                <span class="icon is-medium">
+                    <i class="fab fa-facebook-messenger"></i>
+                </span>
+            </a>
+
+            <a v-bind:href="email" class="button is-rounded is-primary" target="_blank">
+                <span class="icon is-medium">
+                    <i class="fas fa-envelope"></i>
+                </span>
+            </a>
+            <div v-if="isUser">
+                <div v-if="this.editing" v-on:click="toggleEditing()" class=" has-text-centered">
+                    <a class="button is-success is-rounded">Edit</a>
+                </div>
+                <div v-else v-on:click="toggleEditing(); updateFirebase()" class=" has-text-centered ">
+                    <a class="button is-danger is-rounded">Save</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="column">
+        <div class="card-content is-small">
+            <div class="media">
+                <div class="is-mobile is-centered">
+                    <div class="">
+                        <div class="media-content">
+                            <div class="level has-text-weight-bold">
+                                <div class="level-item">
+                                    <p class="title fs-s3 is-capitalized">{{ payload.name }} /</p>
+                                </div>
+                                <div class="level-item">
+                                    <div v-if="this.editing">
+                                        <p class="title fs-s3 ">
+                                             / {{ payload.year }}
+                                        </p>
+                                    </div>
+                                    <div v-else class="control ">
+                                        <div class="select is-focused">
+                                            <select v-model="payload.year">
+                                                <option>Freshmen</option>
+                                                <option>Sophmore</option>
+                                                <option>Junior</option>
+                                                <option>Senior</option>
+                                                <option>Alumni</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="content">
-            <div class="columns is-mobile is-centered">
-                <div class="column">
-                    <div class="fs-s4 has-text-weight-bold">
+            <div class="content">
+                <div class="is-mobile is-centered">
+                    <div class="subtitle fs-s4 has-text-weight-bold">
                         <div v-if="this.editing">Major: {{ payload.major }}</div>
-                        <div v-else>Major: <input v-model="payload.major"></div>
+                        <div v-else class="level">
+                            <div class="level-left">Major: </div>
+                            <div class="level-item">
+                                <input class="input is-focused is-small" type="text" v-model="payload.major">
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="column">
-                    <div class="fs-s4 has-text-weight-bold">
+
+                <div class="is-mobile is-centered">
+                    <div class="subtitle fs-s4 has-text-weight-bold">
                         <div v-if="this.editing">Pledge Class: {{ payload.pledge_class }}</div>
-                        <div v-else>Pledge Class: <input v-model="payload.pledge_class"></div>
+                        <div v-else class="level">
+                            <div class="level-left">Pledge Class:</div>
+                            <div class="level-item">
+                                <input class="input is-focused is-small" type="text" v-model="payload.pledge_class">
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="field is-grouped is-grouped-centered">
-                <p class="control">
-                <a v-bind:href="messengerURL" class="button is-large is-rounded is-info">
-                    <span class="icon is-medium">
-                        <i class="fab fa-facebook-messenger"></i>
-                    </span>
-                </a>
+                <h1 class="fs-s2">About Me</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
-                <p class="control">
-                <a v-bind:href="email" class="button is-large is-rounded is-primary">
-                    <span class="icon is-medium">
-                        <i class="fas fa-envelope"></i>
-                    </span>
-                </a>
+
+                <h1 class="fs-s2">Career Interests</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
             </div>
-            <h1 class="fs-s2">About Me</h1>
-            <p>Hello, I am in KTP - wow!</p>
 
-            <h1 class="fs-s2">Career Interests</h1>
-            <p>I have interests, like many people in KTP</p>
 
 
         </div>
-
-        <div v-if="isUser" class="level buttons are-large">
-            <div v-if="this.editing" v-on:click="toggleEditing()" class="level-item has-text-centered">
-                <a class="button is-success is-rounded">Edit</a>
-            </div>
-            <div v-else v-on:click="toggleEditing(); updateFirebase()" class="level-item has-text-centered">
-                <a class="button is-danger is-rounded">Save</a>
-            </div>
-
-        </div>
-
-    </div>
     </div>
     </div>
     </div>
@@ -144,17 +157,11 @@ export default {
         }
     },
     computed: {
-        isAlum: function() {
-            if (this.year == 'Alumni') {
-                return true;
-            }
-            return false;
-        },
         isUser: function(){
             return (this.$store.state.userData.uniqname === this.uniqname ? true : false);
         },
         email: function() {
-            return 'mailto:' + this.uniqname + '@umich.edu'
+            return 'mailto:' + this.payload.uniqname + '@umich.edu'
         },
         messengerURL: function() {
             return 'https://www.messenger.com/'
@@ -179,6 +186,20 @@ export default {
     padding-left: 50px;
     padding-right: 50px;
   }
+
+  #user-container {
+      padding: 10px;
+  }
+
+  .button-links {
+      padding-top: 10px;
+  }
+  input {
+      margin-left: 10px;
+  }
+
+
+
 </style>
 
 <style>
